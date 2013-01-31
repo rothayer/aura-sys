f43977205d916093209c487a82af2fb67f8efce0
rothayer <thayer_rb@cox.net>
2013-01-31 11:37:26 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuraSys::Application.config.secret_token = '8034827288d1a54e3f73a50fb92f79da6b8a19e7bfbf2ddc52335950d35d5357c2d3b1105b8792957c2963812982e71c2927a2843632bc69ff1e230721c3e283'
